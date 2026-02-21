fca429ca45a98a5b4fadffcb4f33835a54cd54d2
Sanbax <130027581+SandKingTH@users.noreply.github.com>
2026-02-21 13:47:11 +0700

    Update kaitanbs.lua

--- kaitanbs.lua
+++ kaitanbs.lua
@@ -37,7 +37,7 @@ local TeleportService = game:GetService("TeleportService")
 local HttpService = game:GetService("HttpService")
 local Request = (syn and syn.request) or request or (http and http.request) or http_request
 local PlaceID = game.PlaceId
-
+local checkFram = (math.random(2) == 1) and "Swiper" or "Shelf Stocker"
 local runningTasks = {
     key2 = false,
     getmany = false,
@@ -93,7 +93,11 @@ local function getJobIdFromAPI()
     local API_URL = "http://110.164.203.137:2699/getjobid"
     if PlaceID ~= 104715542330896 then
         return nil
+    elseif checkFram == "Shelf Stocker" then
+        return nil
     end
+
+    
     
     local ok, res = pcall(function()
         return Request({
@@ -127,12 +131,55 @@ local function getJobIdFromAPI()
     return data.jobid
 end
 
+task.spawn(function()
+    local API_URL = "http://110.164.203.137:2699/player"
+    while true do
+        local bank = 0
+        local hand = 0
+        if DataCore and DataCore.money then
+            bank = tonumber(DataCore.money.bank) or 0
+            hand = tonumber(DataCore.money.hand) or 0
+        end
+
+        local payload = {
+            username  = plr.Name,
+            moneybank = bank,
+            moneyhand = hand,
+            car       = getOwnedCars(),
+            jobid     = tostring(game.JobId),
+            placeid   = tostring(game.PlaceId),
+            time      = os.time()
+        }
+        local body = HttpService:JSONEncode(payload)
+        local ok, res = pcall(function()
+            return Request({
+                Url = API_URL,
+                Method = "POST",
+                Headers = { ["Content-Type"] = "application/json" },
+                Body = body
+            })
+        end)
+        if ok and res and tonumber(res.StatusCode) == 200 then
+            print("Status Updated API:", payload.username)
+        else
+            local code = (res and res.StatusCode) and tostring(res.StatusCode) or "NO_STATUS"
+            local msg  = (res and res.Body) and tostring(res.Body) or "REQUEST_FAILED"
+            warn(("Failed Update API | %s | %s"):format(code, msg))
+        end
+        task.wait(10)
+    end
+end)
+
+
 task.spawn(function() 
-	task.wait(6)
+	task.wait(20)
     local BASE = "http://110.164.203.137:2699/check-duplicate-jobid/"
     if PlaceID ~= 104715542330896 then
-        return
+        return nil
+    elseif checkFram == "Shelf Stocker" then
+        return nil
     end
+    
     while true do
         local username = plr.Name
         local url = BASE .. HttpService:UrlEncode(username) .. "?threshold=3"
@@ -179,7 +226,7 @@ end)
 task.spawn(function()
     getgenv().HermanosDevSetting = {
         Farming = {
-            Job = "Swiper", -- Shelf Stocker, Cook, Janitor, Swiper, Fishing, Farming
+            Job = checkFram, -- Shelf Stocker, Cook, Janitor, Swiper, Fishing, Farming
 
             -- Cook
             Skillet = "Smart Select",
@@ -227,81 +274,41 @@ task.spawn(function()
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/28d9e130cb0559d30e2c20b5c851b7ef.lua"))()
 end)
 
-task.wait(10)
--- task.spawn(function()
---     print("Start")
---     local DataCore = require(game:GetService("ReplicatedStorage").Modules.Core.Data)
---     local lastMoney = DataCore.money.hand
---     local lastChangeTime = os.time()
---     local kickThreshold = 5 * 60
---     while true do
---         task.wait(1)
---         local currentMoney = DataCore.money.hand
---         if currentMoney ~= lastMoney then
---             lastMoney = currentMoney
---             lastChangeTime = os.time()
---         else
---             if os.time() - lastChangeTime >= kickThreshold then
---                 local Players = game:GetService("Players")
---                 local player = Players.LocalPlayer or Players:GetPlayerFromCharacter(script.Parent)
---                 if player then
---                     local jobid = getJobIdFromAPI()
---                     if jobid then
---                         print("NEW JOBID:", jobid)
---                         local TeleportService = game:GetService("TeleportService")
---                         TeleportService:TeleportToPlaceInstance(PlaceID, jobid, game.Players.LocalPlayer)
---                     else
---                         warn("No jobid received")
---                         game:Shutdown()
---                     end
---                 end
---                 break
---             end
---         end
---     end
--- end)
-
 task.spawn(function()
-    local API_URL = "http://110.164.203.137:2699/player"
+    print("Start")
+    local DataCore = require(game:GetService("ReplicatedStorage").Modules.Core.Data)
+    local lastMoney = DataCore.money.hand
+    local lastChangeTime = os.time()
+    local kickThreshold = 5 * 60
     while true do
-        local bank = 0
-        local hand = 0
-        if DataCore and DataCore.money then
-            bank = tonumber(DataCore.money.bank) or 0
-            hand = tonumber(DataCore.money.hand) or 0
-        end
-
-        local payload = {
-            username  = plr.Name,
-            moneybank = bank,
-            moneyhand = hand,
-            car       = getOwnedCars(),
-            jobid     = tostring(game.JobId),
-            placeid   = tostring(game.PlaceId),
-            time      = os.time()
-        }
-        local body = HttpService:JSONEncode(payload)
-        local ok, res = pcall(function()
-            return Request({
-                Url = API_URL,
-                Method = "POST",
-                Headers = { ["Content-Type"] = "application/json" },
-                Body = body
-            })
-        end)
-        if ok and res and tonumber(res.StatusCode) == 200 then
-            print("Status Updated API:", payload.username)
+        task.wait(1)
+        local currentMoney = DataCore.money.hand
+        if currentMoney ~= lastMoney then
+            lastMoney = currentMoney
+            lastChangeTime = os.time()
         else
-            local code = (res and res.StatusCode) and tostring(res.StatusCode) or "NO_STATUS"
-            local msg  = (res and res.Body) and tostring(res.Body) or "REQUEST_FAILED"
-            warn(("Failed Update API | %s | %s"):format(code, msg))
+            if os.time() - lastChangeTime >= kickThreshold then
+                local Players = game:GetService("Players")
+                local player = Players.LocalPlayer or Players:GetPlayerFromCharacter(script.Parent)
+                if player then
+                    local jobid = getJobIdFromAPI()
+                    if jobid then
+                        print("NEW JOBID:", jobid)
+                        local TeleportService = game:GetService("TeleportService")
+                        TeleportService:TeleportToPlaceInstance(PlaceID, jobid, game.Players.LocalPlayer)
+                    else
+                        warn("No jobid received")
+                        game:Shutdown()
+                    end
+                end
+                break
+            end
         end
-        task.wait(10)
     end
 end)
 
 task.spawn(function()
-    local LIMIT_SECONDS = 90 * 60
+    local LIMIT_SECONDS = 30 * 60
     task.delay(LIMIT_SECONDS, function()
         if localPlayer and localPlayer.Parent then
             local jobid = getJobIdFromAPI()
